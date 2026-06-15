890f0ded69d0e7d21403e8a7547c37543537a154
navizhaaulya <navytzie@gmail.com>
2026-06-15 16:29:29 +0700

    first

---
+++ routes/api.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CrudController;
+// use App\Http\Controllers\CustomController;
+use App\Http\Controllers\UploadController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'middleware' => ['setguard:api', 'auth.rest']
+], function () {
+    Route::get('/{model}', [CrudController::class, 'index']);
+    Route::get('/{model}/dataset', [CrudController::class, 'dataset']);
+    Route::post('/{model}', [CrudController::class, 'create']);
+    Route::put('/{model}/{id}', [CrudController::class, 'update']);
+    Route::delete('/{model}/{id}', [CrudController::class, 'delete']);
+    Route::get('/{model}/{id}', [CrudController::class, 'show']);
+
+    // Route::post('upload', [UploadController::class, 'upload'])->name("upload")->middleware('auth.rest');
+
+
+    Route::get('/gen-lang/lang', [CrudController::class, 'lang']);
+    Route::get('/gen-model/{model}', [CrudController::class, 'generate']);
+    Route::get('/gen-module/listmodule', [CrudController::class, 'listModule']);
+});
+
+Route::group([
+    'middleware' => ['setguard:api']
+], function () {
+    Route::get('file/{model}/{field}/{id}/{time}', [UploadController::class, 'getFile']);
+    Route::get('file/{model}/{field}/{id}/{time}/download', [UploadController::class, 'downloadFile']);
+    Route::get('tumb-file/{model}/{field}/{id}/{time}', [UploadController::class, 'getTumbnailFile']);
+    Route::get('temp-file/{path}/{time}/{ext}', [UploadController::class, 'getTempFile']);
+    Route::get('tumb-temp-file/{path}/{time}/{ext}', [UploadController::class, 'getThumbTempFile']);
+    
+    Route::post('upload', [UploadController::class, 'upload'])->name("upload");
+});
